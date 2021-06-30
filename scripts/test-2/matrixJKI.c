57079540945dac566657566877432e2c1d97a492
Pieter Vorstenbosch <pieter.vorstenbosch@kpnplanet.nl>
2021-06-30 22:08:09 +0200

    Update matrixJKI.c

--- scripts/test-2/matrixJKI.c
+++ scripts/test-2/matrixJKI.c
@@ -28,7 +28,7 @@ gettimeofday(&start, NULL);
 	for (int j=0; j<n; j++) {
 			for (int k=0; k<n; k++) {
 				for (int i=0; i<n; i++) {
-					C[i][j] += A[i][k] * B[k][j]
+					C[i][j] += A[i][k] * B[k][j];
 				}
 			}
 		}
