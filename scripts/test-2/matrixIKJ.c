2ddae52a66f22af1bba787c152b2f8f93ef69692
Pieter Vorstenbosch <pieter.vorstenbosch@kpnplanet.nl>
2021-06-30 22:07:03 +0200

    Update matrixIKJ.c

--- scripts/test-2/matrixIKJ.c
+++ scripts/test-2/matrixIKJ.c
@@ -28,7 +28,7 @@ gettimeofday(&start, NULL);
 	for (int i=0; i<n; i++) {
 			for (int k=0; k<n; k++) {
 				for (int j=0; j<n; j++) {
-					C[i][j] += A[i][k] * B[k][j]
+					C[i][j] += A[i][k] * B[k][j];
 				}
 			}
 		}
