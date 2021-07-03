7819d607123ac160e8dfbf5e2453d5839b0d3c8a
Pieter Vorstenbosch <pieter.vorstenbosch@kpnplanet.nl>
2021-07-03 14:49:53 +0200

    Update and rename matrix_j_loop.c to IJK_matrix_j_loop.c

--- scripts/test-4/IJK_matrix_j_loop.c
+++ scripts/test-4/IJK_matrix_j_loop.c
@@ -27,8 +27,8 @@ struct timeval start;
 struct timeval end;
 gettimeofday(&start, NULL);
 for (int i = 0; i < n; ++i) {
-cilk_for (int k = 0; k < n; ++k) {
-for (int j = 0; j < n; ++j) {
+cilk_for (int j = 0; j < n; ++j) {
+for (int k = 0; k < n; ++k) {
 C[i][j] += A[i][k] * B[k][j];
 }
 }
